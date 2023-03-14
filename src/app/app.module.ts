3118beb3806135600f8be1a5b49ab62807ad303a
rokezalazar <rokezalazar@yahoo.com.ar>
2023-03-13 22:11:21 -0300

    mi cuarto commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoapComponent } from './componentes/logoap/logoap.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { GraficoComponent } from './componentes/grafico/grafico.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    GraficoComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
